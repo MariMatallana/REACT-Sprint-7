3b00a5b42f91d9dc30464d472882e86d2c841dfb
MariMatallana <marianamatallana@gmail.com>
2022-12-27 09:58:23 +0100

    nivel2-ok

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
       <Formulario />
     </div>
 
-
   );
 }
 
--- src/Components/Formulario.js
+++ src/Components/Formulario.js
@@ -1,46 +1,69 @@
-import React, { Fragment } from "react";
+import { calculateNewValue } from "@testing-library/user-event/dist/utils";
+import React, { Fragment, useRef } from "react";
+import { useState } from "react";
+
+let total = 0
 
 const Formulario = () => {
 
-//    const [total, setTotal] = useState({
-//         check1: 500,
-//         check2: 300,
-//         check3: 200,
-//    });
+  const [checkweb, setCheckweb] = useState(false)
+  const [checkseo, setCheckseo] = useState(false)
+  const [checkads, setCheckads] = useState(false)
 
-//    const actualizar = (event) => {
-//     console.log("funciona")
-//     setTotal({
-//         ...total,
-//     })
-//    }
+  function handleCheckweb(myChecked) {
+    if (myChecked.name == "web") {
+      setCheckweb(myChecked.checked)
+      myChecked.checked ? total += Number(myChecked.value) : total -= Number(myChecked.value)
+    }
+  }
 
-    return ( 
-        <Fragment>
-        <h4>¿Qué quieres hacer?</h4>
-        <div className="form-check mt-3">
-        <input className="form-check-input" type="checkbox" value="" name="check1"/>
-          <label className="form-check-label">
-            Una página web (500€)
-          </label>
+  function handleCheckseo(myChecked) {
+    if (myChecked.name == "seo") {
+      setCheckseo(myChecked.checked)
+      myChecked.checked ? total += Number(myChecked.value) : total -= Number(myChecked.value)
+    }
+  }
+
+  function handleChecksads(myChecked) {
+    if (myChecked.name == "googleAdds") {
+      setCheckads(myChecked.checked)
+      myChecked.checked ? total += Number(myChecked.value) : total -= Number(myChecked.value)
+    }
+  }
+
+
+
+  return (
+    <Fragment>
+      <h4>¿Qué quieres hacer?</h4>
+      <div className="form-check mt-3">
+        <input className="form-check-input" type="checkbox" id="web" checked={checkweb} value={500} name="web" onChange={(e) => handleCheckweb(e.target)} />
+        <label className="form-check-label">
+          Una página web (500€)
+        </label>
+      </div>
+      <div className="container mt-3 p-3 form-control border-dark"  width="100px">
+        Numero de páginas <input type="text"/> 
+        <br ></br>
+        Numero de idiomas <input type="text"/>
       </div>
       <div className="form-check mt-3">
-        <input className="form-check-input" type="checkbox" value="" name="check2"/>
-          <label clasName="form-check-label" >
+        <input className="form-check-input" type="checkbox" id="seo" checked={checkseo} value={300} name="seo" onChange={(e) => handleCheckseo(e.target)} />
+        <label className="form-check-label" >
           Una consultoria SEO (300€)
-          </label>
+        </label>
       </div>
       <div className="form-check mt-3">
-        <input className="form-check-input" type="checkbox" value="" name="check3"/>
-          <label className="form-check-label" >
+        <input className="form-check-input" type="checkbox" id="googleAdds" checked={checkads} value={200} name="googleAdds" onChange={(e) => handleChecksads(e.target)} />
+        <label className="form-check-label" >
           Una campaña de Google Ads (200€)
-          </label>
+        </label>
       </div>
       <div className="mt-3">
-        Precio: €
+        Precio: ${total} €
       </div>
-        </Fragment>
-    );
+    </Fragment>
+  );
 }
 
 export default Formulario;
